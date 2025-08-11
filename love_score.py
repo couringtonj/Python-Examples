e686f2c78452adc2bd0d7b71e6bf6f3d214f937c
couringtonj <jonathancourington@outlook.com>
2025-08-11 18:47:23 -0400

    love score project

---
+++ love_score.py
@@ -0,0 +1,37 @@
+def calc_love_score(name1,name2):
+    trimmed_name1=name1.replace(" ","")
+    trimmed_name2=name2.replace(" ","")
+    t = 0
+    r=0
+    u=0
+    e=0
+    l=0
+    o=0
+    v=0
+    e2=0
+    
+    for ltr in (trimmed_name1+trimmed_name2):
+        if ltr=="T" or ltr=="t":
+            t+=1
+        elif ltr=="R" or ltr=="r":
+            r+=1
+        elif ltr=="U" or ltr=="u":
+            u+=1
+        elif ltr=="E" or ltr=="e":
+            e+=1
+        elif ltr=="L" or ltr=="l":
+            l+=1
+        elif ltr=="O" or ltr=="o":
+            o+=1
+        elif ltr=="V" or ltr=="v":
+            v+=1
+        elif ltr=="E" or ltr=="e":
+            e2+1        
+            
+    num1=t+r+u+e
+    num2=l+o+v+e2
+    display(num1,num2)
+def display(num1,num2):
+    print(f"Your love score is {num1}{num2}")
+            
+calc_love_score("Angela Yu","Jack Bauer")
